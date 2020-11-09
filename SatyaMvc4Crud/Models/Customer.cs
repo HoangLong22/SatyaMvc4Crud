51d6f4d4f5643d677cdad1f2d1be46fbdb9a0283
nguyen long <longnguyenhoang26@gmail.com>
2020-11-09 17:43:24 +0700

    chỉnh giao diện

--- SatyaMvc4Crud/Models/Customer.cs
+++ SatyaMvc4Crud/Models/Customer.cs
@@ -12,22 +12,25 @@ namespace SatyaMvc4Crud.Models
         public int CustomerID { get; set; }
 
         [Required(ErrorMessage = "Enter Your Name")]
-        [StringLength(4, ErrorMessage = "Name should be less than or equal to four characters.")]
+        [StringLength(6, ErrorMessage = "Name should be less than or equal to four characters.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Enter Your Address")]
         [StringLength(10, ErrorMessage = "Address should be less than or equal to ten characters.")]
         public string Address { get; set; }
+
         [Required(ErrorMessage = "Your must provide a PhoneNumber")]
         [Display(Name = "Home Phone")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
         public string Mobileno { get; set; }
+
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Enter Your DOB.")]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         [SatyaMvc4Crud.Models.CustomValidationAttributeDemo.ValidBirthDate(ErrorMessage = "Birth Date can not be greater than current date")]
         public DateTime Birthdate { get; set; }
+
         [Required(ErrorMessage = "Enter Your EmailID")]
         [RegularExpression(@"^[\w-\._\+%]+@(?:[\w-]+\.)+[\w]{2,6}$", ErrorMessage = "Please enter a valid email address")]
         public string EmailID { get; set; }
